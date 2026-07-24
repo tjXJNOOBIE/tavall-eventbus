a2e306a0f4afce5c715e3588ef37fbbd11532c49
TJ <tjxjnoobie@gmail.com>
2026-07-24 09:32:16 +0000

    Build: Lock imported test platforms

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,8 @@ repositories {
 
 dependencies {
     api("org.tavall:tavall-logging:1.0.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
